ef78f7656ec1a12f36ade522d4274bf4bad359f8
Cristian von der Heyde <cristian.heyde@gmail.com>
2023-01-26 20:35:28 -0300

    novo

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '8b72e864401c464fa512c165cd466abf',
+  rootNotionPageId: '31433e7531fb4197bb20d901cc34aad5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
